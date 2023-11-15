4a376a2a7302f4ba89bfc0996e5661ed02b905c1
Tristan Dennis <dennistpw@Tristans-LSTM-MBP.local>
2023-11-15 07:53:28 +0000

    tidy fst script

--- bin/analysis/diversity_divergence.R
+++ bin/analysis/diversity_divergence.R
@@ -5,11 +5,39 @@ library(RColorBrewer) #nice colours
 library(kableExtra)
 library(parallel)
 
-####
-#fst and dxy
+##read sample table
+setwd('~/Dropbox (LSTM)/td_je_angam_2022/')
+metadata = read.csv('metadata/sequenced_metadata.csv')
 
-#read fst data
-fstfiles <- list.files(path='~/Projects/td_je_angam_2022/data/fst_by_ecotype_species/', pattern='*win10kstp5k*', full.names = TRUE)
+
+#plottable ecozone names
+metadata$habitatnew <- case_when(
+  metadata$habitat == 'rainforest'~ 'RF',
+  metadata$habitat == 'deciduous_forest'~ 'DF',
+  metadata$habitat == 'mangrove_swamp' ~ 'MS',
+  metadata$habitat == 'coastal_savannah' ~ 'CS'
+)
+
+#plottable species names
+metadata$speciesnew <- case_when(
+  metadata$Form == 'M'~ 'An. col',
+  metadata$Form == 'S'~ 'An. gam s.s.',
+)
+
+#make nice counts table by species and ecoregion
+count(metadata, speciesnew, habitatnew) %>% 
+  kable("html", align = "c", col.names = c("","Ecoregion","N. Samples"), padding=-1L) %>% 
+  kable_styling("striped") %>%
+  collapse_rows() %>% pack_rows(start_row = 1, end_row = 4, group_label = "An. coluzzi", italic = TRUE) %>% 
+  pack_rows(start_row = 5, end_row = 5, group_label = "An. gambiae", italic = TRUE) %>% 
+  remove_column(1)
+
+
+####
+#fst
+
+#read and wrangle fst data...
+fstfiles <- list.files(path='~/Dropbox (LSTM)/td_je_angam_2022/data/fst_by_ecotype_species/', pattern='*win10kstp5k*', full.names = TRUE)
 fstdata  <- mclapply(fstfiles, function(x) data.table(fread(x), 'name' = basename(x))) #read data
 fstdata <- do.call(rbind, fstdata)
 colnames(fstdata) <- c('bit','chrom','midpos','nSites','fst','name')
@@ -18,25 +46,10 @@ colnames(fstdata) <- c('bit','chrom','midpos','nSites','fst','name')
 fstdata$name <-gsub('form','',fstdata$name)
 fstdata <- fstdata %>% separate(name, c('species_a','ecozone_a','species_b', 'ecozone_b', 'chrpref','chrom','suffix'))
 
-#read dxy data
-dxyfiles <- list.files(path='~/Projects/td_je_angam_2022/data/dxy/', pattern='*windowed.txt', full.names = TRUE)
-dxydata  <- mclapply(dxyfiles, function(x) data.table(fread(x), 'name' = basename(x))) #read data
-dxydata <- do.call(rbind, dxydata)
-colnames(dxydata) <- c('bit','chrom','midpos','dxy','name')
-
-#sort comp levels for fsts
-dxydata$name <-gsub('.dxy.windowed.txt','',dxydata$name)
-dxydata$name <-gsub('decid_forest','decidforest',dxydata$name)
-unique(dxydata$name)
-dxydata <- dxydata %>% separate(name, c('species_a','ecozone_a','species_b', 'ecozone_b'))
-
 
 #fiuddle with names for df merge
-dxydatasub <- dxydata[c('chrom','midpos','dxy','species_a','species_b', 'ecozone_a','ecozone_b')]
-colnames(dxydatasub) <- c('chrom','midpos','stat','species_a','species_b', 'ecozone_a','ecozone_b')
 fstdatasub <- fstdata[c('chrom','midpos','fst','species_a','species_b', 'ecozone_a','ecozone_b')]
 colnames(fstdatasub) <- c('chrom','midpos','stat','species_a','species_b', 'ecozone_a','ecozone_b')
-mean(fstdata$nSites)
 
 #need ro swap values for ecozone a and b for species comp gam and col
 # Condition to select rows for which you want to swap values
@@ -50,16 +63,16 @@ fstdatasub$ecozone_a[condition] <- fstdatasub$ecozone_b[condition]
 fstdatasub$ecozone_b[condition] <- temp_values
 
 #add stat type to dxy and fst
-dxydatasub$stat_type <- 'dxy'
 fstdatasub$stat_type <- 'fst'
 
 #temporary keep only gam
 #bind
-specstats <- rbind(dxydatasub, fstdatasub)
+specstats <- fstdatasub #rbind(dxydatasub, fstdatasub)
 
 #chrom id tidy
 specstats$chrom <- gsub('AgamP4_','',specstats$chrom) #tidy chrid
 
+##horrible code to change ecoregion and species names
 
 #plottable ecozone names
 specstats$ecozone_a_s <- case_when(
@@ -82,33 +95,37 @@ specstats$species_a <- case_when(
   specstats$species_a == 'm'~ 'An. col',
   specstats$species_a == 's'~ 'An. gam s.s.',
 )
+
 specstats$species_b <- case_when(
   specstats$species_b == 'm'~ 'An. col',
   specstats$species_b == 's'~ 'An. gam s.s.',
 )
 
-#chrom ordering
+#chrom ordering for plots
 specstats$chrom <- factor(specstats$chrom, levels=c('2R','2L','3R','3L','X'))
 
-#spec_comp 
+#make factor for species comparison level
 specstats$spec_comp <- paste0(specstats$species_a, ':', specstats$species_b)
-#habitat_comp 
+#make factor for ecoregion comparison level
 specstats$hab_comp <- paste0(specstats$ecozone_a_s, ':', specstats$ecozone_b_s)
 
 #################################################
 #identify outliers in data
 #################################################
 
+#function for getting mode of data
 getmode <- function(v) {
   uniqv <- unique(v)
   uniqv[which.max(tabulate(match(v, uniqv)))]
 }
 
-
+#define outlierthreshold based on mode + 3x mode of distribution of fst (see Lucas 2023 nature comms)
 get_outlier_threshold <- function(column){
-  outlierserthreshold <- getmode(column) + (3*(getmode(column) - min(column)))
+  outlierthreshold <- getmode(column) + (3*(getmode(column) - min(column)))
+  return(outlierthreshold)
 }
 
+#based on above, define whether a window is an outlier window or not
 specstats <- specstats %>% 
   filter(stat_type == 'fst') %>% 
   group_by(spec_comp, hab_comp) %>% 
@@ -116,7 +133,7 @@ specstats <- specstats %>%
          new_column = ifelse(stat > modal_value, "Outlier", "NotOutlier"))
 
 #################################################
-#plot fsts
+#plot fst genome scans
 #################################################
 
 m_fsts <- specstats %>% filter(stat_type=='fst' & hab_comp != 'CS:DF' & hab_comp != 'RF:CS' & hab_comp != 'RF:DF' & species_a == 'An. col' & species_b == 'An. col') %>% 
@@ -160,18 +177,17 @@ s_fsts <- specstats %>% filter(stat_type=='fst' & hab_comp != 'CS:DF' & hab_comp
 #plot thetas
 #################################################
 
-thetas <- list.files(path='~/Projects/td_je_angam_2022/data/thetas/', pattern='.*indow*', full.names = TRUE)
+thetas <- list.files(path='~/Dropbox (LSTM)/td_je_angam_2022/data/thetas/', pattern='.*indow*', full.names = TRUE)
 ecozones <- gsub('_nomaffilter.thetasWindow.gz.pestPG','',basename(thetas))
+
 #function plot thetas per ecozone and species
 thetafilelist <- mclapply(thetas, function(x) data.table(fread(x), 'ecozone' = x)) #read data
 thetafilelist <- mclapply(thetafilelist, function(x) x[x$Chr %like% "AgamP4_2" | x$Chr %like% "AgamP4_3" | x$Chr %like% "AgamP4_X", ]) #subset to chroms of interest
 thetafilelist <- do.call(rbind, thetafilelist)
 
-
+#fiddle with the different delimiters to make nice ecoregion and species names from filenames
 thetafilelist$ecozone <- gsub('_nomaffilter.thetasWindow.gz.pestPG','',basename(thetafilelist$ecozone))
-
 thetafilelist$ecozone <- gsub('_nomaf.*','',basename(thetafilelist$ecozone))
-
 thetafilelist$ecozone <- gsub('decid_forest','decidforest',basename(thetafilelist$ecozone))
 thetafilelist$species <- gsub('_.*','',basename(thetafilelist$ecozone))
 thetafilelist$ecozone <- gsub('*._','',basename(thetafilelist$ecozone))
@@ -185,22 +201,22 @@ thetafilelist$ecozone <- case_when(
   thetafilelist$ecozone == 'savannah' ~ 'CS'
 )
 
+#plottable species names
 thetafilelist$species <- case_when(
   thetafilelist$species == 'm'~ 'An. col',
   thetafilelist$species == 's'~ 'An. gam s.s.',
 )
 
-  #order ecozones and chgroms
+#order ecozones and chroms
 thetafilelist$ecozone <- factor(thetafilelist$ecozone, levels=c('DF','RF','CS', 'MS'))
 thetafilelist$Chr <- factor(thetafilelist$Chr, levels=c('AgamP4_2R','AgamP4_2L','AgamP4_3R','AgamP4_3L','AgamP4_X'))
 thetafilelist <- thetafilelist[thetafilelist$Chr != 'Chr'] #remove accidential headers lol
   
-  #make coords for plotting x axis facet sizes (so x axes limits per chrom facet are lengths proportionate to actual sizes)
-maxchromlengths <- aggregate(WinCenter ~ Chr, thetafilelist, max)
-
-#calculate pi
+#normalise thetapi and thetahat by nsites
 thetafilelist$Pi <- thetafilelist$tP / thetafilelist$nSites
+thetafilelist$theta <- thetafilelist$tW / thetafilelist$nSites
 
+#plot coluzzi thetas
 m_thetas <- thetafilelist %>% filter(species == 'An. col') %>% 
   pivot_longer(cols=c('Tajima','Pi')) %>% 
   ggplot(aes(x=WinCenter,y=value,colour=ecozone))+
@@ -214,7 +230,7 @@ m_thetas <- thetafilelist %>% filter(species == 'An. col') %>%
           panel.spacing = unit(0.1, "lines"),
           axis.text.x = element_text(angle=45, vjust=0.5, hjust=0.7))
 
-thetafilelist$theta <- thetafilelist$tW / thetafilelist$nSites
+#plot gambiae thetas
 s_thetas <- thetafilelist %>% filter(species != 'An. col') %>% 
   pivot_longer(cols=c('Tajima','Pi')) %>% 
   ggplot(aes(x=WinCenter,y=value,colour=ecozone))+
@@ -227,10 +243,12 @@ s_thetas <- thetafilelist %>% filter(species != 'An. col') %>%
   theme(strip.text.x = element_blank(),
         panel.spacing = unit(0.1, "lines"),
         axis.text.x = element_text(angle=45, vjust=0.5, hjust=0.7))
-s_thetas
+
+#extract legends
 mthetaslegend <- cowplot::get_legend(m_thetas)
 sthetaslegend <- cowplot::get_legend(s_thetas)
-plot(mthetaslegend)
+
+#plot n save plots fst and thetas 2geh4
 mplots <- cowplot::plot_grid(m_fsts, m_thetas+theme(legend.position = "none"),nrow=2, ncol=1,align = 'v', axis='bt', labels = c('A','B'))
 ggsave('~/Projects/td_je_angam_2022/figures/fst_coluzzi_byecology.tiff', mplots, width = 18, height = 20, units = 'cm')
 
@@ -241,8 +259,6 @@ ggsave('~/Projects/td_je_angam_2022/figures/fst_gambiae_byecology.tiff', splots,
 ####################################################################################################
 #now let's make nice thetas table to summarise data
 ####################################################################################################
-
-
 thetafilelist$pi <- thetafilelist$tP/thetafilelist$nSites
 thetafilelist$Ne <- thetafilelist$tW / 3.5e-09 / thetafilelist$nSites / 4 # calculate effective population size
 thetatab <- thetafilelist[,c('Chr','ecozone', 'species', 'Tajima','pi', 'Ne')]
@@ -267,7 +283,6 @@ thetatab %>% arrange(Ecozone) %>%
 ####################################################################################################
 #now let's make nice fst table to summarise fsts
 ####################################################################################################
-
 specstats %>% filter(stat_type=='fst' & hab_comp != 'CS:DF' & hab_comp != 'RF:CS' & hab_comp != 'RF:DF') %>% 
  # filter(species_a =='An. col' & species_b =='An. col') %>% 
   group_by(species_a, species_b, ecozone_a_s, ecozone_b_s) %>% summarise(Fst = mean(stat)) 
@@ -282,17 +297,10 @@ specstats %>% filter(stat_type=='fst' & hab_comp != 'CS:DF' & hab_comp != 'RF:CS
   add_header_above(c("Ecozone A" = 1,"Ecozone B"=2))
 ?add_header_above  
 
-
 ####################################################################################################
 #intersect outlier fst windows with gene data for supplementary tables
 ####################################################################################################
-#prepare bed format 
-s = winfst %>% filter(outlier == 'outlier') #get only outliers > 99.9th %ile
-s = mutate(s, start = pos -500, end = pos + 500) #make start stop coords for bed file (from window midpoint)
-s = select(s, chr, start, end, piece, fst, form, id)#select bed cols
-s = s[order(chr, start, end)] #order our windows
-
-
+#prepare bed format from fst
 beddf <- specstats %>% filter(stat_type=='fst' & new_column == 'Outlier' & hab_comp != 'CS:DF' & hab_comp != 'RF:CS' & hab_comp != 'RF:DF' & spec_comp != 'An. col:An. gam s.s.') %>% 
   mutate(start = midpos - 5000) %>% 
   mutate(end = midpos + 5000) %>% 
@@ -306,28 +314,12 @@ genebed = genebed[order(V1, V4),] #sort just in case
 colnames(genebed) = c('chrom', 'start', 'end', 'name') #add colnames
 genebed$chrom <- gsub('AgamP4_' , '',genebed$chrom)
 
+#intersect and write table
 outlierwindow_genes = tidygenomics::genome_intersect(beddf, genebed, c("chrom"="chrom", "start"="start", "end"="end")) #intersect
-write_csv(outlierwindow_genes, '~/Projects/td_je_angam_2022/data/annotation/outlier_window_genes.csv')
-
-
+write_csv(outlierwindow_genes, '~/Projects/td_je_angam_2022/data/annotation/outlier_window_genes.csv') #write as table
 
 outlierwindow_genes = data.table(outlierwindow_genes)
-
+#how many are in 2La?
 outlierwindow_genes %>% filter(chrom == '2L' & start > 20528089 & end <42165182)
 
-max_windows = outlierwindow_genes[outlierwindow_genes[, .I[which.max(stat)], by=list(name)]$chrom]
 
-
-max_windows = max_windows[- grep("unspecified", max_windows$name),] 
-winfst = max_windows[winfst, on = 'id']
-
-winfst$id = gsub(";.*","",winfst$name)
-winfst$description = gsub(".*=","",winfst$name)
-winfst$description = gsub("\\[.*","",winfst$description)
-#winfst$text <- paste("Gene:", winfst$description,"\nChromosome: ", winfst$i.chr, "\nWindow: ", winfst$pos, "\nFst:", winfst$i.fst %>% round(2), sep="")
-
-#write final fst df to a compressed csv
-#write.csv(x, file=gzfile('/Users/tristanpwdennis/Projects/MOVE/data/anopheles_03_21_DW/angsd-output/windowed_stats_1kb/fsts_windowed_byecotype.csv.gz'))
-#winfst= fread('fsts_windowed_byecotype.csv.gz')
-winfst = data.table(winfst)[,c('angsd_rubbish', 'text', 'ecoa', 'ecob', 'i.chr', 'i.piece', 'i.fst', 'i.form', 'i.name', 'i.start'):=NULL]
-#write.csv(winfst, file=gzfile('/Users/tristanpwdennis/Projects/MOVE/data/anopheles_03_21_DW/angsd-output/windowed_stats_1kb/fsts_windowed_byecotype.csv.gz'))
